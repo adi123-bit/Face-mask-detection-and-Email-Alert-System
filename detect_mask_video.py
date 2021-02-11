8528dc4f6fa75611f9106c3c6a7b7be1a78dcd61
Aditya Sawant <60505090+adi123-bit@users.noreply.github.com>
2021-02-11 14:58:35 +0530

    Update detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -155,7 +155,7 @@ while True:
                                    subtype="jpg", filename=fname)
 
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                smtp.login('2018.aditya.sawant@ves.ac.in', '@a15a31a17')
+                smtp.login('2018.aditya.sawant@ves.ac.in', '')
                 smtp.send_message(msg)
             print('[INFO] alert mail Sent to authorities')
         elif(label == 'Mask'):
